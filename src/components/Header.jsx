2bfbe6144e277974de67e50ec2a525877c814c4d
Samson <82292757+De-Slorex1@users.noreply.github.com>
2026-05-11 20:03:24 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
 
                         <h1 className="heading-primary">
                                 <span className="heading-primary--main">✨Jacob Samson</span>
-                                <span className="heading-primary--sub styling_head" style={{fontWeight: '900'}}>WEB FULL-STACK DEVELOPER</span>
+                                <span className="heading-primary--sub styling_head" style={{fontWeight: '900'}}>FRONTEND DEVELOPER</span>
                         </h1>
 
                         <a href="https://netorgft16459046-my.sharepoint.com/:w:/g/personal/onlineclasses_itssng_com/EXqtovWQeK9AqTa5NpPgpFMBvIY4HSt-pBzjIwtu-7NeRQ?e=CKyLQh" target="_blank" rel="noreferrer" className="resume_button btn btn--animated ">DOWNLOAD MY RESUME</a>
